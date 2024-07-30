9559bad70ecfbea6893233640202f60612820894
Andrey Karev <karevandrey.work@gmail.com>
2024-07-30 14:12:42 +0300

    Created project structure

---
+++ src/main/java/studyprojects/atm/ATM.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package studyprojects.atm;
+
+/**
+ *
+ * @author Andrey Karev
+ */
+public class ATM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
